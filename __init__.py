c360b25891631cd872c5557a3c05bd763bcd598b
Mertcan Ekiz <ekiz.mertcan@gmail.com>
2019-06-13 21:55:49 +0300

    Fixed formatting & removed unused imports

--- __init__.py
+++ __init__.py
@@ -1,10 +1,9 @@
 from flask import Flask
 from flask_pymongo import PyMongo
-from flask_login import LoginManager
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'd67aee21ea3581a60ed7b2cf43f384a983b2e23ac1cee232'
 app.config['MONGO_URI'] = 'mongodb://127.0.0.1:27017/cart'
 db = PyMongo(app).db
 
-import routes
--- routes.py
+++ routes.py
@@ -1,10 +1,12 @@
 from __init__ import app
 from flask import render_template
 
+
 @app.route('/')
 @app.route('/home')
 def hello_world():
     return render_template('home.html')
+
 
 @app.route('/test/<var>')
 def test(var):
