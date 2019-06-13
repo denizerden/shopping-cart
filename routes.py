7929c05f399c4e41da8d5439dba1039f13fd5c72
Mertcan Ekiz <ekiz.mertcan@gmail.com>
2019-06-13 15:07:21 +0300

    Added basic templates

---
+++ routes.py
@@ -0,0 +1,11 @@
+from __init__ import app
+from flask import render_template
+
+@app.route('/')
+@app.route('/home')
+def hello_world():
+    return render_template('home.html')
+
+@app.route('/test/<var>')
+def test(var):
+    return render_template('test.html', var=var)
