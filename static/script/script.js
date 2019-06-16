8de736059e6b99dc7dfb9c63126b78042d8c4533
denizerden <denizerden@sabanciuniv.edu>
2019-06-17 00:35:59 +0300

    plus,minus button added

--- static/script/script.js
+++ static/script/script.js
@@ -54,3 +54,9 @@ function removeItem(item) {
     }
     saveCart();
 }
+
+$('.btn-minuse').on('click', function(){            $(this).parent().siblings('input').val(parseInt($(this).parent().siblings('input').val()) - 1)
+})
+
+$('.btn-pluss').on('click', function(){            $(this).parent().siblings('input').val(parseInt($(this).parent().siblings('input').val()) + 1)
+})
