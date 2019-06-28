a0fd188cfdc03ee44cd3641e13ffd353e098831f
denizerden <denizerden@sabanciuniv.edu>
2019-06-28 14:44:01 +0300

    color field modified

--- static/script/newProduct.js
+++ static/script/newProduct.js
@@ -19,56 +19,58 @@ $(document).ready(function() {
     );
   }
 
-  $("#product_type").change(function(event) {
-    let type = this.value;
-    $("#product_properties_group").removeClass("d-none");
-    $("#product_properties").html(``);
-    for (prop of PRODUCT_TYPES[type].properties) {
-      $("#product_properties").append(
-        `<div class="form-check">
-          <input class="form-check-input" type="checkbox" value="${prop}" id="check-${prop}">
-          <label class="form-check-label" for="check-${prop}">${prop.toUpperCase()}</label>
-        </div>
-        `
-      );
-    }
-
-    $("#check-color").change(function () {
+    $("#product_type").change(function(event) {
+        let type = this.value;
+        $("#product_properties_group").removeClass("d-none");
+        $("#product_properties").html(``);
+        for (prop of PRODUCT_TYPES[type].properties) {
+          $("#product_properties").append(
+            `<div class="form-check">
+              <input class="form-check-input" type="checkbox" value="${prop}" id="check-${prop}">
+              <label class="form-check-label" for="check-${prop}">${prop.toUpperCase()}</label>
+            </div>
+            `
+          );
+        }
       let i;
-      if (this.checked == true) {
-        i=0;
-        $("#add-new-color").removeClass("d-none");
-        $("#color-selection").prepend('<div class="mt-1" ><input type=\'text\' id="custom" /> <input class="form-control" type=\'text\' id="color-name" placeholder="Color Name" /><input class="form-control" type=\'number\' id="color-price" placeholder="Price" step="0.1" /> </div> ');
-        $("#custom").spectrum({
-          color: "#f00",
-          replacerClassName:'form-control'
+        $("#check-color").change(function () {
+
+          if (this.checked == true) {
+            i=0;
+            $("#add-new-color").removeClass("d-none");
+            /*$("#color-selection").prepend(`<div id="color-selection-${i}" class="mt-1" ><input type=\'text\' id="custom" /> <input class="form-control" type=\'text\' id="color-name" placeholder="Color Name" /><input class="form-control" type=\'number\' id="color-price" placeholder="Price" step="0.1" /> </div> `);
+            $("#custom").spectrum({
+              color: "#f00",
+              replacerClassName:'form-control'
+            });*/
+
+          }
+          else{
+            //$("#color-selection").html('');
+              $(`.color-select`).remove();
+            i=0;
+            $("#add-new-color").addClass("d-none");
+          }
         });
         $("#add-new-color").click(function (event) {
           event.preventDefault();
           i++;
-          $("#color-selection").prepend(`<div class="mt-1"> <input type=\'text\' id="custom-${i}" /> <input class="form-control" type=\'text\' id="color-name" placeholder="Color Name" /><input class="form-control" type=\'number\' id="color-price" placeholder="Price" step="0.1" /> </div>`);
+          $("#color-selection").prepend(`<div class="color-select mt-1"> <input type=\'text\' id="custom-${i}" /> <input class="form-control" type=\'text\' id="color-name" placeholder="Color Name" /><input class="form-control" type=\'number\' id="color-price" placeholder="Price" step="0.1" /> </div>`);
           $(`#custom-${i}`).spectrum({
             color: "#f00",
             replacerClassName:'form-control'
           });
 
         });
-      }
-      else{
-        $("#color-selection").html('');
-        i=0;
-        $("#add-new-color").addClass("d-none");
-      }
-    });
-    $("#check-size").change(function () {
-      if (this.checked == true) {
-        alert("checked")
-      } else {
-        console.log($("#custom").spectrum("get"));
-      }
-    });
+        $("#check-size").change(function () {
+          if (this.checked == true) {
+            alert("checked")
+          } else {
+            console.log($("#custom").spectrum("get"));
+          }
+        });
 
-  });
+    });
 
 
 
