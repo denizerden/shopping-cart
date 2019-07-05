ad0a34aa76390962943b5e53dafa5c1518668c12
denizerden <denizerden@sabanciuniv.edu>
2019-07-05 11:02:07 +0300

    newProduct updated

--- static/script/newProduct.js
+++ static/script/newProduct.js
@@ -1,3 +1,35 @@
+
+let colorArray = [];
+let optionCount = 0;
+function newForm(id) {
+  return `
+  <div class="form-group mx-sm-3 mb-2">
+  <select class="form-control" id="form-${id}">
+      <option>Renk</option>
+      <option>2</option>     
+  </select>
+</div>
+`;
+}
+function addOption (id) {
+  optionCount++;
+  $('#product-form').append(newForm(id));
+  console.log(id)
+  $(`#form-${id}`).change(function(event){
+    let element = event.target;
+    console.log(element)
+    let selectedOption = element.options[element.selectedIndex].value;
+  
+    if(selectedOption==="Renk"){
+      console.log("renk seçildi");
+      $("#color-picker").removeClass("d-none");
+      }
+    else if(selectedOption==="2"){
+      $("#color-picker").addClass("d-none");
+    }
+  });
+ 
+}
 $(document).ready(function() {
   $("#product-form").submit(function(event) {
     event.preventDefault();
@@ -18,4 +50,56 @@ $(document).ready(function() {
         console.log(data);
       });
   });
+  const pickr = Pickr.create({
+    el: '.pickr',
+    
+    swatches: [
+        'rgba(244, 67, 54, 1)',
+        'rgba(233, 30, 99, 0.95)',
+        'rgba(156, 39, 176, 0.9)',
+        'rgba(103, 58, 183, 0.85)',
+        'rgba(63, 81, 181, 0.8)',
+        'rgba(33, 150, 243, 0.75)',
+        'rgba(3, 169, 244, 0.7)',
+        'rgba(0, 188, 212, 0.7)',
+        'rgba(0, 150, 136, 0.75)',
+        'rgba(76, 175, 80, 0.8)',
+        'rgba(139, 195, 74, 0.85)',
+        'rgba(205, 220, 57, 0.9)',
+        'rgba(255, 235, 59, 0.95)',
+        'rgba(255, 193, 7, 1)'
+    ],
+
+    components: {
+
+        // Main components
+        preview: true,
+        opacity: true,
+        hue: true,
+
+        // Input / output Options
+        interaction: {
+            hex: true,
+            rgba: true,
+            hsla: true,
+            hsva: true,
+            cmyk: true,
+            input: true,
+            clear: true,
+            save: true
+        }
+    }
 });
+
+pickr.on('save', (color, instance) => {
+ 
+  colorArray.push(color);
+  console.log(colorArray);
+});
+// Add new option
+$('#add-new-option').click(event => {
+  console.log("test");
+  addOption(`option-${optionCount}`);
+});
+});
+
