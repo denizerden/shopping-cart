b2a59df2134ddd09c0d5414c3bd7e129b01fe47f
Mertcan Ekiz <ekiz.mertcan@gmail.com>
2019-07-07 21:28:43 +0300

    Fixed Product model and updated templates

--- models.py
+++ models.py
@@ -1,17 +1,36 @@
 from flask_login import UserMixin
 from mongoengine.document import Document, EmbeddedDocument
 from mongoengine.fields import (
-    DateTimeField, DecimalField, EmailField, EmbeddedDocumentField,
-    EmbeddedDocumentListField, IntField, ListField, ObjectIdField,
-    ReferenceField, StringField)
+    BooleanField, DateTimeField, DecimalField, EmailField,
+    EmbeddedDocumentField, EmbeddedDocumentListField, IntField, ListField,
+    ObjectIdField, ReferenceField, StringField, LazyReferenceField)
+
+
+class Option(Document):
+    option_id = StringField(required=True)
+    option_type = StringField(required=True)
+    option_text = StringField(required=True)
+    option_value = StringField(required=True)
+
+
+class Price(Document):
+    valid_from = DateTimeField(required=True)
+    valid_to = DateTimeField(required=True)
+    currency = StringField(required=True)
+    original_price = DecimalField(min_value=0, precision=2)
+    discounted_price = DecimalField(min_value=0, precision=2)
+    discount_rate = DecimalField(min_value=0, max_value=1, precision=4)
+    stock = IntField(min_value=0, required=True)
+    is_active = BooleanField(required=True)
+    options = ListField(LazyReferenceField(Option))
 
 
 class Product(Document):
     title = StringField(required=True)
     description = StringField(required=True)
-    price = DecimalField(required=True, force_string=True)
-    image_file = StringField()
+    image_url = StringField()
     created_on = DateTimeField(auto_now_add=True)
+    prices = ListField(LazyReferenceField(Price))
 
 
 class CartItem(EmbeddedDocument):
