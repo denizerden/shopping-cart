cb3b5dead6837ff05eae48f96411b3f82ebdcf93
denizerden <denizerden@sabanciuniv.edu>
2019-07-09 13:30:12 +0300

    color button added

--- static/script/newProduct.js
+++ static/script/newProduct.js
@@ -1,6 +1,6 @@
 
 let colorArray = [];
-let optionCount = 1;
+let optionCount = 0;
 
 function newForm(id) {
   return `
@@ -10,7 +10,7 @@ function newForm(id) {
                                 <option>Renk</option>
                                 <option>2</option>     
                             </select></td>
-                            <td><button class="btn btn-primary" type="submit" id="add-new-option">Seçenek Ekle</button></td>
+                            <td id="option-details-${id}"><button class="btn btn-primary float-right" type="button" id="add-new-option-${id}">Seçenek Ekle</button></td>
                         </tr>
 `;
 }
@@ -25,15 +25,67 @@ function addOption (id) {
   
     if(selectedOption==="Renk"){
       console.log("renk seçildi");
-     
+      $(`#option-details-${id}`).append(`<div id="pickr-${id}"><div class="pickr-${id}"> </div></div>`)
+      const pickr = Pickr.create({
+            el: `.pickr-${id}`,
+            
+            swatches: [
+                'rgba(244, 67, 54, 1)',
+                'rgba(233, 30, 99, 0.95)',
+                'rgba(156, 39, 176, 0.9)',
+                'rgba(103, 58, 183, 0.85)',
+                'rgba(63, 81, 181, 0.8)',
+                'rgba(33, 150, 243, 0.75)',
+                'rgba(3, 169, 244, 0.7)',
+                'rgba(0, 188, 212, 0.7)',
+                'rgba(0, 150, 136, 0.75)',
+                'rgba(76, 175, 80, 0.8)',
+                'rgba(139, 195, 74, 0.85)',
+                'rgba(205, 220, 57, 0.9)',
+                'rgba(255, 235, 59, 0.95)',
+                'rgba(255, 193, 7, 1)'
+            ],
+        
+            components: {
+        
+                // Main components
+                preview: true,
+                opacity: true,
+                hue: true,
+        
+                // Input / output Options
+                interaction: {
+                    hex: true,
+                    rgba: true,
+                    hsla: true,
+                    hsva: true,
+                    cmyk: true,
+                    input: true,
+                    clear: true,
+                    save: true
+                }
+            }
+        });
+        
+        pickr.on('save', (color, instance) => {
+         
+          colorArray.push(color);
+          console.log(colorArray);
+        });
+        
     }
     else if(selectedOption==="2"){
-      //$("#color-picker").addClass("d-none");
-    }
+      $(`#pickr-${id}`).remove();
+        }
   });
- 
+  $(`#add-new-option-${id}`).click(event => {
+    console.log("test");
+    addOption(`${optionCount}`);
+    
+  });
 }
 $(document).ready(function() {
+  addOption(0)
   $("#product-form").submit(function(event) {
     event.preventDefault();
     data = {
@@ -101,11 +153,12 @@ $(document).ready(function() {
 // });
 // Add new option
 let i =1;
-$('#add-new-option').click(event => {
-  //console.log("test");
-  addOption(`option-${optionCount}`);
+// $(`#add-new-option-0`).click(event => {
+//   //console.log("test");
+//   addOption(`${optionCount}`);
   
-});
+// });
+
 
 });
 
