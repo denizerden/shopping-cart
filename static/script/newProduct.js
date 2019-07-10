666e6046a0c8beed4da9320d52cdf5ed6aa4df9c
Mertcan Ekiz <ekiz.mertcan@gmail.com>
2019-07-10 11:46:26 +0300

    Added color and price form

--- static/script/newProduct.js
+++ static/script/newProduct.js
@@ -1,6 +1,6 @@
-
 let colorArray = [];
 let optionCount = 0;
+let priceCount = 0;
 
 function newForm(id) {
   return `
@@ -14,159 +14,233 @@ function newForm(id) {
                         </tr>
 `;
 }
-function addOption (id) {
+
+function newPrice(id) {
+  return `
+        <div class="card-body" id="price-form-${id}">
+          <div class="container">
+            <div class="row mt">
+              <div class="col"><input type="text" id="date-from-${id}" class="datepicker-here form-control" placeholder="Başlangıç Tarihi" data-language="en"></div>
+              <div class="col"><input type="text" id="date-to-${id}" class="datepicker-here form-control" placeholder="Bitiş Tarihi" data-language="en"></div>
+            </div>
+            <div class="row mt-4">
+              <div class="w-100"></div>
+              <div class="col"> <input type="number" id="normal-price-${id}" class="form-control" placeholder="Normal Fiyat" step="0.01"></div>
+              <div class="col"> <input type="number" id="discounted-price-${id}" class="form-control" placeholder="İndirimli Fiyat" step="0.01"></div>
+            </div> 
+            <div class="row mt-4">
+              <div class="w-100"></div>
+              <div class="col"> <input type="number" class="form-control" placeholder="İndirim Oranı" id="discount-rate-${id}"></div>
+              <div class="col"></div>
+            </div>   
+          </div>
+        </div>
+  `;
+}
+
+function addPrice(id) {
+  priceCount++;
+  $('#collapseTwo').append(newPrice(id));
+}
+
+function addOption(id) {
   optionCount++;
   $('#dynamic-field').append(newForm(id));
   //console.log(id)
-  $(`#form-${id}`).change(function(event){
+  $(`#form-${id}`).change(function(event) {
     let element = event.target;
     //console.log(element)
     let selectedOption = element.options[element.selectedIndex].value;
-  
-    if(selectedOption==="Renk"){
+
+    if (selectedOption === 'Renk') {
       $(`#other-${id}`).remove();
-      console.log("renk seçildi");
-      $(`#option-details-${id}`).append(`<div id="pickr-${id}" class="input-group-sm  d-inline-flex" ><div class="pickr-${id}"> </div>
+      console.log('renk seçildi');
+      $(`#option-details-${id}`)
+        .append(`<div id="pickr-${id}" class="input-group-sm  d-inline-flex" ><div class="pickr-${id}"> </div>
 
       <input type="text" class="form-control ml-2" id="color-name-${id}" placeholder="Renk Adı" aria-label="Username" aria-describedby="basic-addon1">
-    </div>`)
+    </div>`);
       const pickr = Pickr.create({
-            el: `.pickr-${id}`,
-            
-            swatches: [
-                'rgba(244, 67, 54, 1)',
-                'rgba(233, 30, 99, 0.95)',
-                'rgba(156, 39, 176, 0.9)',
-                'rgba(103, 58, 183, 0.85)',
-                'rgba(63, 81, 181, 0.8)',
-                'rgba(33, 150, 243, 0.75)',
-                'rgba(3, 169, 244, 0.7)',
-                'rgba(0, 188, 212, 0.7)',
-                'rgba(0, 150, 136, 0.75)',
-                'rgba(76, 175, 80, 0.8)',
-                'rgba(139, 195, 74, 0.85)',
-                'rgba(205, 220, 57, 0.9)',
-                'rgba(255, 235, 59, 0.95)',
-                'rgba(255, 193, 7, 1)'
-            ],
-        
-            components: {
-        
-                // Main components
-                preview: true,
-                opacity: true,
-                hue: true,
-        
-                // Input / output Options
-                interaction: {
-                    hex: true,
-                    rgba: true,
-                    hsla: true,
-                    hsva: true,
-                    cmyk: true,
-                    input: true,
-                    clear: true,
-                    save: true
-                }
-            }
-        });
-        
-        pickr.on('save', (color, instance) => {
-         
-          colorArray.push(color);
-          console.log(colorArray);
-        });
-        
-    }
-    else if(selectedOption==="Diğer"){
+        el: `.pickr-${id}`,
+
+        swatches: [
+          'rgba(244, 67, 54, 1)',
+          'rgba(233, 30, 99, 0.95)',
+          'rgba(156, 39, 176, 0.9)',
+          'rgba(103, 58, 183, 0.85)',
+          'rgba(63, 81, 181, 0.8)',
+          'rgba(33, 150, 243, 0.75)',
+          'rgba(3, 169, 244, 0.7)',
+          'rgba(0, 188, 212, 0.7)',
+          'rgba(0, 150, 136, 0.75)',
+          'rgba(76, 175, 80, 0.8)',
+          'rgba(139, 195, 74, 0.85)',
+          'rgba(205, 220, 57, 0.9)',
+          'rgba(255, 235, 59, 0.95)',
+          'rgba(255, 193, 7, 1)',
+        ],
+
+        components: {
+          // Main components
+          preview: true,
+          opacity: true,
+          hue: true,
+
+          // Input / output Options
+          interaction: {
+            hex: true,
+            rgba: true,
+            hsla: true,
+            hsva: true,
+            cmyk: true,
+            input: true,
+            clear: true,
+            save: true,
+          },
+        },
+      });
+
+      pickr.on('save', (color, instance) => {
+        colorArray.push(color);
+        console.log(colorArray);
+      });
+    } else if (selectedOption === 'Diğer') {
       $(`#pickr-${id}`).remove();
-      $(`#option-details-${id}`).append(`<div id="other-${id}" class="input-group-inline  d-inline-flex" >
+      $(`#option-details-${id}`)
+        .append(`<div id="other-${id}" class="input-group-inline  d-inline-flex" >
 
       <input type="text" class="form-control ml-2" id="other-option-${id}" placeholder="Seçenek" aria-label="Username" aria-describedby="basic-addon1">
-    </div>`)
-        }
+    </div>`);
+    }
   });
   $(`#add-new-option-${id}`).click(event => {
-    console.log("test");
+    console.log('test');
     addOption(`${optionCount}`);
-    
   });
 }
 $(document).ready(function() {
   addOption(0);
-  $("#product-form").submit(function(event) {
+  $(
+    `#option-details-0`
+  ).append(`<div id="pickr-0" class="input-group-sm  d-inline-flex" ><div class="pickr-0"> </div>
+
+      <input type="text" class="form-control ml-2" id="color-name-0" placeholder="Renk Adı" aria-label="Username" aria-describedby="basic-addon1">
+    </div>`);
+  const pickr = Pickr.create({
+    el: `.pickr-0`,
+
+    swatches: [
+      'rgba(244, 67, 54, 1)',
+      'rgba(233, 30, 99, 0.95)',
+      'rgba(156, 39, 176, 0.9)',
+      'rgba(103, 58, 183, 0.85)',
+      'rgba(63, 81, 181, 0.8)',
+      'rgba(33, 150, 243, 0.75)',
+      'rgba(3, 169, 244, 0.7)',
+      'rgba(0, 188, 212, 0.7)',
+      'rgba(0, 150, 136, 0.75)',
+      'rgba(76, 175, 80, 0.8)',
+      'rgba(139, 195, 74, 0.85)',
+      'rgba(205, 220, 57, 0.9)',
+      'rgba(255, 235, 59, 0.95)',
+      'rgba(255, 193, 7, 1)',
+    ],
+
+    components: {
+      // Main components
+      preview: true,
+      opacity: true,
+      hue: true,
+
+      // Input / output Options
+      interaction: {
+        hex: true,
+        rgba: true,
+        hsla: true,
+        hsva: true,
+        cmyk: true,
+        input: true,
+        clear: true,
+        save: true,
+      },
+    },
+  });
+
+  pickr.on('save', (color, instance) => {
+    colorArray.push(color);
+    console.log(colorArray);
+  });
+  addPrice(0);
+  $('#product-form').submit(function(event) {
     event.preventDefault();
     data = {
-      lorem: "ipsum",
-      dolor: "sit",
-      amet: 1299
+      lorem: 'ipsum',
+      dolor: 'sit',
+      amet: 1299,
     };
     fetch(document.URL, {
-      method: "POST",
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json"
+        'Content-Type': 'application/json',
       },
-      body: JSON.stringify(data)
+      body: JSON.stringify(data),
     })
       .then(response => response.json())
       .then(data => {
         console.log(data);
       });
   });
-//   const pickr = Pickr.create({
-//     el: '.pickr',
-    
-//     swatches: [
-//         'rgba(244, 67, 54, 1)',
-//         'rgba(233, 30, 99, 0.95)',
-//         'rgba(156, 39, 176, 0.9)',
-//         'rgba(103, 58, 183, 0.85)',
-//         'rgba(63, 81, 181, 0.8)',
-//         'rgba(33, 150, 243, 0.75)',
-//         'rgba(3, 169, 244, 0.7)',
-//         'rgba(0, 188, 212, 0.7)',
-//         'rgba(0, 150, 136, 0.75)',
-//         'rgba(76, 175, 80, 0.8)',
-//         'rgba(139, 195, 74, 0.85)',
-//         'rgba(205, 220, 57, 0.9)',
-//         'rgba(255, 235, 59, 0.95)',
-//         'rgba(255, 193, 7, 1)'
-//     ],
+  //   const pickr = Pickr.create({
+  //     el: '.pickr',
 
-//     components: {
+  //     swatches: [
+  //         'rgba(244, 67, 54, 1)',
+  //         'rgba(233, 30, 99, 0.95)',
+  //         'rgba(156, 39, 176, 0.9)',
+  //         'rgba(103, 58, 183, 0.85)',
+  //         'rgba(63, 81, 181, 0.8)',
+  //         'rgba(33, 150, 243, 0.75)',
+  //         'rgba(3, 169, 244, 0.7)',
+  //         'rgba(0, 188, 212, 0.7)',
+  //         'rgba(0, 150, 136, 0.75)',
+  //         'rgba(76, 175, 80, 0.8)',
+  //         'rgba(139, 195, 74, 0.85)',
+  //         'rgba(205, 220, 57, 0.9)',
+  //         'rgba(255, 235, 59, 0.95)',
+  //         'rgba(255, 193, 7, 1)'
+  //     ],
 
-//         // Main components
-//         preview: true,
-//         opacity: true,
-//         hue: true,
+  //     components: {
 
-//         // Input / output Options
-//         interaction: {
-//             hex: true,
-//             rgba: true,
-//             hsla: true,
-//             hsva: true,
-//             cmyk: true,
-//             input: true,
-//             clear: true,
-//             save: true
-//         }
-//     }
-// });
+  //         // Main components
+  //         preview: true,
+  //         opacity: true,
+  //         hue: true,
 
-// pickr.on('save', (color, instance) => {
- 
-//   colorArray.push(color);
-//   console.log(colorArray);
-// });
-// Add new option
-let i =1;
-// $(`#add-new-option-0`).click(event => {
-//   //console.log("test");
-//   addOption(`${optionCount}`);
-  
-// });
+  //         // Input / output Options
+  //         interaction: {
+  //             hex: true,
+  //             rgba: true,
+  //             hsla: true,
+  //             hsva: true,
+  //             cmyk: true,
+  //             input: true,
+  //             clear: true,
+  //             save: true
+  //         }
+  //     }
+  // });
 
+  // pickr.on('save', (color, instance) => {
 
+  //   colorArray.push(color);
+  //   console.log(colorArray);
+  // });
+  // Add new option
+  let i = 1;
+  // $(`#add-new-option-0`).click(event => {
+  //   //console.log("test");
+  //   addOption(`${optionCount}`);
+
+  // });
 });
-
