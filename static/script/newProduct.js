0aba9cc503716d8c6811752e2afeb25649de994b
Mertcan Ekiz <ekiz.mertcan@gmail.com>
2019-07-10 14:42:21 +0300

    Added color picker on page load

--- static/script/newProduct.js
+++ static/script/newProduct.js
@@ -1,9 +1,6 @@
 let colorArray = [];
 let optionCount = 0;
 let priceCount = 0;
-let npChange = false;
-let dpChange = false;
-let drChange = false;
 
 function calc(id) {
   let dpChanged = $(`#discounted-price-${id}`).data('changed');
@@ -87,23 +84,6 @@ function addColorSelect(id) {
   const pickr = Pickr.create({
     el: `.pickr-0`,
 
-    swatches: [
-      'rgba(244, 67, 54, 1)',
-      'rgba(233, 30, 99, 0.95)',
-      'rgba(156, 39, 176, 0.9)',
-      'rgba(103, 58, 183, 0.85)',
-      'rgba(63, 81, 181, 0.8)',
-      'rgba(33, 150, 243, 0.75)',
-      'rgba(3, 169, 244, 0.7)',
-      'rgba(0, 188, 212, 0.7)',
-      'rgba(0, 150, 136, 0.75)',
-      'rgba(76, 175, 80, 0.8)',
-      'rgba(139, 195, 74, 0.85)',
-      'rgba(205, 220, 57, 0.9)',
-      'rgba(255, 235, 59, 0.95)',
-      'rgba(255, 193, 7, 1)',
-    ],
-
     components: {
       // Main components
       preview: true,
@@ -133,6 +113,7 @@ function addColorSelect(id) {
 function addOption(id) {
   optionCount++;
   $('#dynamic-field').append(newForm(id));
+  addColorSelect(id);
   //console.log(id)
   $(`#form-${id}`).change(function(event) {
     let element = event.target;
